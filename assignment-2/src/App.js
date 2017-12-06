7a90e25f88823c3ce54d4beb5b63cd8f7b3212e9
muchukotavijay <Vijaya.Reddy@news.com.au>
2017-12-06 11:55:40 +1100

    Correcting the directories

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,29 +1,71 @@
 import React, { Component } from 'react';
-import UserInput from './components/UserInput/UserInput';
-import UserOutput from './components/UserOutput/UserOutput';
 import './App.css';
+import ValidationComponent from './components/ValidationComponent/ValidationComponent';
+import CharComponent from './components/CharComponent/CharComponent';
 
 class App extends Component {
   state = {
-    username : 'Bill'
+    inputText: '',
+    inputTextLength: 0
   }
 
-  changeUserNameHandler = (event) => {
-    this.setState(
-      {
-        username : event.target.value
-      }
-    )
+  printLengthHandler = (event) => {
+    const inputText = event.target.value;
+
+    const inputTextLength = inputText.split('').length;
+    
+    this.setState({
+      inputText: inputText,
+      inputTextLength : inputTextLength
+    })
+
   }
+
+  deleteCharHandler = (index) => {
+    const inputText = [...this.state.inputText.split('')];
+    inputText.splice(index, 1);
+    const resultText = inputText.join('');
+    this.setState({
+      inputText: resultText,
+      inputTextLength: resultText.length
+    })
+
+  }
+
   render() {
+    const style = {
+        inputStyle : {
+            width: '60%',
+            padding: '12px 20px',
+            margin: '8px 0',
+            display: 'inline-block',
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            boxSizing: 'border-box',
+            boxShadow: '10px 10px 5px #888888'
+        }
+    }
+
+    let inputText = this.state.inputText;
+    let blocks = '';
+
+    blocks = (
+      <div>
+        { inputText.split('').map ( (character, index) => 
+            <CharComponent 
+              key = {character+index}
+              character = {character}
+              clicked = { () => { this.deleteCharHandler(index) }}
+            />) }
+      </div>
+    );
+
     return (
       <div className="App">
-        <UserInput changed = {this.changeUserNameHandler} username = {this.state.username}/>
-        <UserOutput username = {this.state.username}/>
-        {/* <UserOutput />
-        <UserOutput />
-        <UserOutput />
-        <UserOutput /> */}
+        <input type='text' style={style.inputStyle} onChange = { (event) => this.printLengthHandler(event)} value={this.state.inputText}/>
+        <p> {this.state.inputTextLength} </p>
+        <ValidationComponent textLength= {this.state.inputTextLength} />
+        {blocks}
       </div>
     );
   }
