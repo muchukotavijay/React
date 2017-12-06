638ebc88fcb27edbf7d5fceff34ca294e8b5a6a8
muchukotavijay <Vijaya.Reddy@news.com.au>
2017-12-06 12:34:56 +1100

    updated App.js

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -5,20 +5,13 @@ import CharComponent from './components/CharComponent/CharComponent';
 
 class App extends Component {
   state = {
-    inputText: '',
-    inputTextLength: 0
+    inputText: ''
   }
 
   printLengthHandler = (event) => {
-    const inputText = event.target.value;
-
-    const inputTextLength = inputText.split('').length;
-    
     this.setState({
-      inputText: inputText,
-      inputTextLength : inputTextLength
+      inputText: event.target.value
     })
-
   }
 
   deleteCharHandler = (index) => {
@@ -26,8 +19,7 @@ class App extends Component {
     inputText.splice(index, 1);
     const resultText = inputText.join('');
     this.setState({
-      inputText: resultText,
-      inputTextLength: resultText.length
+      inputText: resultText
     })
 
   }
@@ -46,26 +38,22 @@ class App extends Component {
         }
     }
 
-    let inputText = this.state.inputText;
-    let blocks = '';
-
-    blocks = (
-      <div>
-        { inputText.split('').map ( (character, index) => 
-            <CharComponent 
-              key = {character+index}
-              character = {character}
-              clicked = { () => { this.deleteCharHandler(index) }}
-            />) }
-      </div>
-    );
+    const charBlocks  = this.state.inputText.split('').map( (ch, index) => {
+      return (
+        <CharComponent
+          key = {ch+index} 
+          character = {ch}
+          clicked = { () => { this.deleteCharHandler(index) }} />
+        )
+    });
 
     return (
       <div className="App">
         <input type='text' style={style.inputStyle} onChange = { (event) => this.printLengthHandler(event)} value={this.state.inputText}/>
-        <p> {this.state.inputTextLength} </p>
-        <ValidationComponent textLength= {this.state.inputTextLength} />
-        {blocks}
+        <p> {this.state.inputText} </p>
+        <p> {this.state.inputText.length} </p>
+        <ValidationComponent textLength= {this.state.inputText.length} />
+        {charBlocks}
       </div>
     );
   }
